b54651555f13055706dd074ed7c3c84e3b4dd263
Dleosilva04 <diogoleosilva04@gmail.com>
2024-11-30 19:45:07 -0300

    Upload Projeo Github

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById('formCadastro').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const comentario = document.getElementById('comentario').value;
+
+    if (coment
